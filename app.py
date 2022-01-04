2c799334b2da83002e39fe06b67ceb730971ca20
minjae cha <cmjj0824@naver.com>
2022-01-04 20:47:34 +0900

    fourth commit

--- app.py
+++ app.py
@@ -32,6 +32,13 @@ def view_reviews():
     reviews = list(db.review.find({}, {'_id': False}))
     return jsonify({'all_reviews': reviews})
 
+# 리뷰 삭제하기
+@app.route('/delete', methods=['POST'])
+def delete_review():
+    name_receive = request.form['name_give']
+    db.review.delete_one({'name': name_receive})
+    return jsonify({'msg': '삭제 완료!'})
+
 # 서버
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
